76e5e3cf2376468e2de897c12f39a5f0b90e0f5a
Michael Hajný <47132583+3n0wd3n@users.noreply.github.com>
2023-06-14 14:58:33 +0200

    Create pig_latin.py

---
+++ pig_latin.py
@@ -0,0 +1,20 @@
+def pig_it(text):
+    new_tense = ""
+    txt = text.split()
+    for idx, word in enumerate(txt):
+        first_letter = word[0]
+        word = word[1:]
+        if len(word) == 0:
+            if not first_letter in "?!" and len(word) == 0:
+                word += first_letter + "ay" + " "
+            if first_letter in "?!":
+                word += first_letter
+            new_tense += word
+        else:
+            word += first_letter + "ay" + " "
+            new_tense += word
+    if "?" in new_tense or "!" in new_tense:
+        new_tense = new_tense[0:len(new_tense)]
+    else:
+        new_tense = new_tense[0:len(new_tense) - 1]
+    return new_tense
