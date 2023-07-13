d08143e9008b4fe53841db57928efaa92585ada7
Michael Hajný <47132583+3n0wd3n@users.noreply.github.com>
2023-07-13 15:42:17 +0200

    Update scramblies.py

--- scramblies.py
+++ scramblies.py
@@ -1,3 +1,15 @@
+"""
+Complete the function scramble(str1, str2) that returns true if a portion of str1 characters can be rearranged to match str2, otherwise returns false.
+
+Notes:
+
+Only lower case letters will be used (a-z). No punctuation or digits will be included.
+Performance needs to be considered.
+Examples
+scramble('rkqodlw', 'world') ==> True
+scramble('cedewaraaossoqqyt', 'codewars') ==> True
+scramble('katas', 'steak') ==> False
+"""
 # def scramble(s1, s2):
 #     counter = 0
 #     for char in s2:
