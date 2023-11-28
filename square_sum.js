d87924821af6415d57c79160501f24e6bfad21ec
Michael Hajný <47132583+3n0wd3n@users.noreply.github.com>
2023-11-28 14:54:07 +0100

    Create square_sum.js

---
+++ square_sum.js
@@ -0,0 +1,16 @@
+/*
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+For example, for [1, 2, 2] it should return 9.
+*/
+
+function squareSum(numbers){
+  if (numbers.length === 0){
+    return 0;
+  }
+  let counter = 0; 
+  for (let i = 0; i < numbers.length; i++) {
+    counter += numbers[i] ** 2;
+  }
+  return counter;
+}
